f672f3270fe7cb0f4d9ed16719ceb433d01c0bdc
faycal <fayssoule@yahoo.com>
2023-11-15 16:29:31 +0100

    responsive nav-foot

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,39 +1,42 @@
-'use client'
-
-import { Box, Container, Stack, Text, useColorModeValue } from '@chakra-ui/react'
+import { Box, Container, Stack} from '@chakra-ui/react';
 
 export default function SmallWithNavigation() {
   return (
-    <Box
-      bg={'white'}
-      color={'black'}>
+    <Box bg={'white'} color={'black'}>
       <Container
-      fontFamily={'Courier New'}
+        fontFamily={'Courier New'}
         as={Stack}
         maxW={'6xl'}
         py={4}
         direction={{ base: 'column', md: 'row' }}
         spacing={4}
         justify={{ base: 'center', md: 'space-between' }}
-        align={{ base: 'center', md: 'center' }}>
-        <Stack direction={'row'} ml={'71%'} spacing={6}>
-          <Box as="a" href={'https://www.instagram.com/lesvh_/'} target="_blank"
-          _hover={{color: '#A3ABB8'}}>
+        align={{ base: 'center', md: 'center' }}
+      >
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          spacing={{ base: 4, md: 6 }}
+          align={{ base: 'center', md: 'center' }}
+        >
+          <Box
+            as="a"
+            href={'https://www.instagram.com/lesvh_/'}
+            target="_blank"
+            _hover={{ color: '#A3ABB8' }}
+          >
             Instagram
           </Box>
-          <Box as="a" href={'#'}
-           _hover={{color: '#A3ABB8'}}>
+          <Box as="a" href={'#'} _hover={{ color: '#A3ABB8' }}>
             About
           </Box>
-          <Box as="a" href={'#'} _hover={{color: '#A3ABB8'}}>
+          <Box as="a" href={'#'} _hover={{ color: '#A3ABB8' }}>
             Contact
           </Box>
         </Stack>
-        <Box as='a'href='#'
-        _hover={{color: '#A3ABB8'}}>
-        Les VH
+        <Box as="a" href={'/'} _hover={{ color: '#A3ABB8' }}>
+          Les VH
         </Box>
       </Container>
     </Box>
-  )
+  );
 }
--- src/components/navbar/nav.tsx
+++ src/components/navbar/nav.tsx
@@ -13,10 +13,13 @@ import {
   MenuDivider,
   useDisclosure,
   useColorModeValue,
+  useMediaQuery,
   Stack,
   useColorMode,
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon, AddIcon } from '@chakra-ui/icons';
+
+
 
 interface Props {
   href: string;
@@ -42,6 +45,7 @@ const NavLink = (props: Props) => {
 
 export default function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isLargerThanMd] = useMediaQuery('(min-width: 48em)'); 
 
   return (
     <>
@@ -57,17 +61,39 @@ export default function Navbar() {
           <HStack spacing={8} alignItems={'center'} mb={6}>
             <Box flex={1} display="flex"> 
             </Box>
+            {isLargerThanMd && (
             <HStack as={'nav'} spacing={10} display={{ base: 'none', md: 'flex' }}>
               <NavLink href={'/'}>HOME</NavLink>
               <NavLink href={'products'}>SHOP</NavLink>
               <NavLink href={'#dashboard'}>COLLECTIONS</NavLink>
               <NavLink href={'#dashboard'}>CART</NavLink>
             </HStack>
+            )}
+            {isLargerThanMd && (
             <HStack as={'nav'} spacing={10} ml={990} display={{base: 'none', md: 'flex'}}>
             <NavLink href={'/'}>LES VH</NavLink>
             </HStack>
+            )}
           </HStack>
         </Flex>
+        {isOpen && (
+          <Flex
+            direction="column"
+            position="absolute"
+            top="60px"
+            left="0"
+            right="0"
+            bg="black"
+            p={4}
+            display={{ md: 'none' }}
+          >
+            <NavLink href={'/'}>HOME</NavLink>
+            <NavLink href={'products'}>SHOP</NavLink>
+            <NavLink href={'#dashboard'}>COLLECTIONS</NavLink>
+            <NavLink href={'#dashboard'}>CART</NavLink>
+            <NavLink href={'/'}>LES VH</NavLink>
+          </Flex>
+        )}
       </Box>
     </>
   );
